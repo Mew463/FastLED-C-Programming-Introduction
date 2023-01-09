54eb7da7b102df5be47c0d9cdfb3e48425d57fb0
Mew463 <72902803+Mew463@users.noreply.github.com>
2023-01-08 20:42:34 -0800

    added a space

--- FastLED Programming Introduction/FastLED Programming Introduction.ino
+++ FastLED Programming Introduction/FastLED Programming Introduction.ino
@@ -7,12 +7,12 @@ CRGB leds[NUM_LEDS];
 
 void setup() { // Stuff in here runs once
   /* FIX ME: What code to enable the Serial monitor? */
+
   FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip ); // Initialize the led strip
   FastLED.setBrightness(100);                                                             // set the brightness (0 - 100)
 
   turnLedOn(CRGB::Blue, 5); // Example function call. 
    
-
 
 }
 void loop() { // Stuff in here runs forever
